813389e1ad6f4dc6fe6489ecd75429d859125950
Prisha Gupta <prishapg101@gmail.com>
2024-03-17 09:47:08 +0530

    Added user input for user query

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
-import { readFileSync } from "fs"
+import { writeFileSync } from "fs"
+import { createInterface } from "readline/promises"
 import { closeDBConnection } from "./core/neo4j"
-import { insertDataIntoDB } from "./database/node"
+
+// publishRelease
+
+function writeJSON(file: string, data: any) {
+	writeFileSync(`${file}.data.json`, JSON.stringify(data, null, 2))
+}
 
 async function main() {
-	await insertDataIntoDB(JSON.parse(readFileSync("nodes.data.json", "utf-8"))).then(
-		() => closeDBConnection()
-	)
+	// await insertDataIntoDB(JSON.parse(readFileSync("nodes.data.json", "utf-8"))).then(
+	// 	() => closeDBConnection()
+	// )
+
+	const readline = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+	const query = await readline.question("Enter your query: ")
+	readline.close()
+
+	console.log(query)
+
+	closeDBConnection()
 }
 
 main()
