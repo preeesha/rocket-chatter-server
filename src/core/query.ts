7ecc86fa91185cc0a14c64cdb232acf726ebdee7
Prisha Gupta <prishapg101@gmail.com>
2024-03-28 21:54:01 +0530

    Modularized DB query using the new embeddings

--- src/core/query.ts
+++ src/core/query.ts
@@ -1,34 +1,51 @@
 import { DB_QUERY_BASE_PROMPT } from "../constants"
+import { DBNode } from "../database/node.types"
 import { LLM } from "./llm"
 import { db } from "./neo4j"
 
 export namespace Query {
+	export async function getDBNodesFromVectorQuery(
+		indexName: string,
+		vector: number[],
+		threshold: number
+	): Promise<DBNode[]> {
+		const result = await db.run(
+			`
+				CALL db.index.vector.queryNodes("${indexName}", 2, $vector)
+				YIELD node, score 
+				WHERE score >= ${threshold}
+				WITH node, score // Include the score here
+				MATCH (node)-[r]->(relatedNode)
+				RETURN node, COLLECT(relatedNode) AS relatedNodes, score
+				ORDER BY score DESC
+			`,
+			{ vector }
+		)
+
+		const data = result.records.map((record) => record.toObject())
+		const results: any[] = []
+		for (const record of data) {
+			const n = record.node.properties
+			delete n["nameEmbeddings"]
+			delete n["codeEmbeddings"]
+			results.push(n)
+		}
+
+		return results
+	}
+
 	export async function getCodeNodesFromKeywords(
 		keywords: string[]
-	): Promise<any[]> {
-		const results: any[] = []
+	): Promise<DBNode[]> {
+		const results: DBNode[] = []
 		for (const keyword of keywords) {
-			const queryText = await LLM.generateEmbeddings(keyword)
-			const result = await db.run(
-				`
-					CALL db.index.vector.queryNodes("embeddings", 2, $queryText)
-					YIELD node, score
-					WHERE score >= 0.9
-					WITH node
-					MATCH (node)-[r]->(relatedNode)
-					RETURN node, COLLECT(relatedNode) AS relatedNodes
-				`,
-				{ queryText: queryText }
+			const queryVector = await LLM.generateEmbeddings(keyword)
+			const result = await getDBNodesFromVectorQuery(
+				"nameEmbeddings",
+				queryVector,
+				0.9
 			)
-
-			const data = result.records.map((record) => record.toObject())
-			if (!data.length) continue
-
-			for (const record of data) {
-				const n = record.node.properties
-				delete n["embeddings"]
-				results.push(n)
-			}
+			results.push(...result)
 		}
 
 		return results
