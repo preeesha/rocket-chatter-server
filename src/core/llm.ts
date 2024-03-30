dfd88f32844870160ad73c787f31bfe9a659d145
Prisha Gupta <prishapg101@gmail.com>
2024-03-30 11:57:00 +0530

    Added `Prompt` class for advanced prompting

--- src/core/llm.ts
+++ src/core/llm.ts
@@ -4,6 +4,46 @@ import { HF_KEY, LLM_MODEL, OPENAI_KEY } from "../constants"
 
 const openai = new OpenAI({ apiKey: OPENAI_KEY })
 
+export class Prompt {
+	private systemPrompt: string
+	private assistantPrompt: string
+
+	constructor(systemPrompt: string, assistantPrompt: string) {
+		this.systemPrompt = systemPrompt
+		this.assistantPrompt = assistantPrompt
+	}
+
+	/* TODO: Add format checking or use langchain */
+	make(
+		messages: { role: "system" | "assistant" | "user"; content: string }[]
+	): string {
+		let prompt = "<s>\n"
+		prompt += `\t[INST]\n\t\t${this.systemPrompt}\n\t[/INST]\n\t`
+		prompt += this.assistantPrompt
+		prompt += "\n</s>\n"
+
+		for (let i = 0; i < messages.length; i++) {
+			const message = messages[i]
+			prompt += `\t[INST]\n\t\t${message.content}\n\t[/INST]`
+
+			const nextMessage = messages[++i]
+			if (!nextMessage) {
+				break
+			}
+
+			prompt += `\n\t${nextMessage.content}`
+			prompt += "\n</s>\n"
+		}
+		prompt += " Assistant:"
+
+		return prompt
+	}
+
+	static processOutput(output: string): string {
+		return (output.split("[/INST] Assistant:").at(-1) ?? "").trim()
+	}
+}
+
 export namespace LLM {
 	export async function generateEmbeddings(name: string): Promise<number[]> {
 		const content = await openai.embeddings.create({
